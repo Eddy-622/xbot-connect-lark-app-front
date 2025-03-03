800113d12e3e741c4c8bcd53220888de4a49d8f4
蓝湛 <lan.zhan@yingdao.com>
2025-03-03 20:31:44 +0800

    新页面，补充链接地址

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
   const [userId, setUserId] = useState("");
   const [tenantKey, setTenantKey] = useState("");
 
-  const [categories, setCategories] = useState<string[]>([]);
+  const [selectSource, setSelectSource] = useState<string>("");
   const [form] = Form.useForm();
 
   useEffect(() => {
@@ -35,8 +35,10 @@ export default function App() {
     bitable.saveConfigAndGoNext(config);
   };
 
-  const useDocLink = "#";
-  const helpDeskLink = "#";
+  const useDocLink =
+    "https://ying-dao.feishu.cn/docx/LIFada2LWo657WxurYGcHV4GnYe";
+  const helpDeskLink =
+    "https://ying-dao.feishu.cn/wiki/R6uswOyV3iy9vLkfNzlckuJBnuc?fromScene=bitTable";
 
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "95vh" }}>
@@ -54,23 +56,41 @@ export default function App() {
           autoComplete="off"
         >
           <Form.Item
-            name="user_sec_id"
-            label="user id"
-            rules={[{ required: true, len: 76 }]}
-          >
-            <Input placeholder="抖音主页链接中的ID" />
-          </Form.Item>
-
-          <Form.Item
             name="data_category"
             label="数据来源"
             rules={[{ required: true }]}
           >
-            <Select placeholder="选择一个数据来源" allowClear>
+            <Select
+              placeholder="选择一个数据来源"
+              allowClear
+              onChange={(value) => setSelectSource(value)}
+              defaultValue="user_profile"
+            >
               <Option value="user_profile">用户主页数据</Option>
               <Option value="user_video_works">用户视频作品</Option>
+              <Option value="video_comments">抖音视频评论</Option>
             </Select>
           </Form.Item>
+
+          {/* 根据不同的selectSource弹出不同的选项 */}
+
+          {selectSource === "video_comments" ? (
+            <Form.Item
+              name="aweme_id"
+              label="视频ID/modal_id"
+              rules={[{ required: true, len: 19 }]}
+            >
+              <Input placeholder="抖音视频链接中的ID" />
+            </Form.Item>
+          ) : (
+            <Form.Item
+              name="user_sec_id"
+              label="sec_user_id"
+              rules={[{ required: true, len: 76 }]}
+            >
+              <Input placeholder="抖音主页链接中的ID" />
+            </Form.Item>
+          )}
 
           <Flex justify="right">
             <Space size="small">
@@ -88,11 +108,11 @@ export default function App() {
       <Flex className="footer" justify="center" align="flex-end">
         <div style={{ color: "grey" }}>
           由影刀数据连接器提供技术服务
-          <a href={helpDeskLink}>
+          <a href={helpDeskLink} target="_blank">
             <UserOutlined />
             <span>付费定制</span>
           </a>
-          <a href={useDocLink}>
+          <a href={useDocLink} target="_blank">
             <span>&nbsp;使用说明</span>
           </a>
         </div>
